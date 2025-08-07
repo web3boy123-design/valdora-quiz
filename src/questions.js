11e02f9552eab363aeb663ed07f3f0f3227c3629
web3boy123-design <web3boy123@gmail.com>
2025-08-07 18:43:49 +0500

    Create questions.js

---
+++ src/questions.js
@@ -0,0 +1,8 @@
+// 📁 src/questions.js
+export const questions = [
+  'What is liquid staking?',
+  'What is Valdora Finance?',
+  'Which chain is Valdora building on?',
+  'How can users exit staking faster with Valdora?',
+  'Which DEX supports trading of stZIG?'
+];
