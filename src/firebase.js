4c2e9af99be6af90ab3dfcc561bbc0a1c0529a1a
web3boy123-design <web3boy123@gmail.com>
2025-08-07 19:01:14 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDdDJwge9ELOsOgzQER0RC5cL_1mvwxbw0",
+  authDomain: "valdora-test-quiz.firebaseapp.com",
+  projectId: "valdora-test-quiz",
+  storageBucket: "valdora-test-quiz.firebasestorage.app",
+  messagingSenderId: "383006582624",
+  appId: "1:383006582624:web:1805c08152ae5aa30ca005",
 };
 
 const app = initializeApp(firebaseConfig);
